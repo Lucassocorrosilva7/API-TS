f6bf1085a1188017916bd013461fa3da108e94c4
lukecode <lucassocorrosilva@gmail.com>
2022-10-13 17:39:09 -0300

    Add libs dotenv

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config'
 import express from 'express'
 import 'express-async-errors'
 import cors from 'cors'
@@ -10,6 +11,6 @@ app.get('/', (req, res) => {
   return res.json({ message: 'Olá dev' })
 })
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000! ⚡')
+app.listen(process.env.PORT, () => {
+  console.log(`Server started on port ${process.env.PORT}! ⚡`)
 })
