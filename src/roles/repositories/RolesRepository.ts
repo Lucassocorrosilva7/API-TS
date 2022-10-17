c7936b947afa21218d03318978a96de1e1f5da29
lukecode <lucassocorrosilva@gmail.com>
2022-10-17 12:02:04 -0300

    Criando método de lista

--- src/roles/repositories/RolesRepository.ts
+++ src/roles/repositories/RolesRepository.ts
@@ -11,11 +11,15 @@ export class RolesRepository {
     this.roles = []
   }
 
-  create({ name }: CreateRoleDTO) {
+  create({ name }: CreateRoleDTO): Role {
     const role = new Role()
     Object.assign(role, { name, created_at: new Date() })
 
     this.roles.push(role)
     return role
+  }
+
+  findAll(): Role[] {
+    return this.roles
   }
 }
