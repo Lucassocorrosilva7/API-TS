494578b7213cf584d2e50e1b691a77c1ee50b85f
lukecode <lucassocorrosilva@gmail.com>
2022-10-31 11:29:26 -0300

    Refatorando role entity

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,9 +1,10 @@
+import { Role } from '@roles/entities/Role'
 import { DataSource } from 'typeorm'
 import { CreateRolesTable1666381127031 } from './migrations/1666381127031-CreateRolesTable'
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
-  entities: [],
+  entities: [Role],
   migrations: [CreateRolesTable1666381127031],
 })
